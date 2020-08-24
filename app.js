57a11e0621efb782dbec3c5fc2e1da23136ae831
Marius_haru <roberto_marius12@yahoo.com>
2020-08-24 17:44:55 +0300

    Level 5 : session and passport

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session")
+const passport = require("passport")
+const passportLocalMongoose = require("passport-local-mongoose")
 
 const app = express();
 const port = 3000;
@@ -14,16 +15,39 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+// initializare session
+app.use(session({
+    secret: 'Our little secret.',
+    resave: false,
+    saveUninitialized: false
+    
+  }))
+
+// initializare si folosire a passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//mongoose 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology:true})
+mongoose.set("useCreateIndex", true)
 
 const userSchema = new mongoose.Schema({
     email:String,
     password:String
 });
 
+// initalizare passport-local-mongoose
+userSchema.plugin(passportLocalMongoose);
+
 
 // modelu mongoose
 const User = new mongoose.model("User",userSchema)
+
+// initializare si folosire a passport-local-mongoose
+passport.use(User.createStrategy());
+ 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req,res){
     res.render("home")
@@ -37,24 +61,32 @@ app.get("/register",function(req,res){
     res.render("register")
 })
 
+app.get("/secrets", function(req,res){
+    if(req.isAuthenticated()){
+        res.render("secrets")
+    }else{
+        res.render("login")
+    }
+})
+
+app.get("/logout", function(req,res){
+    req.logout();
+    res.redirect("/");
+})
+
+
 app.post("/register", function(req,res){
 
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        // Store hash in your password DB.
-        // prea info. transmise in register
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        })
-        // salveaza info. in baza de date
-        newUser.save(function(err){
-            if(err){
-                console.log(err);
-            }else{
-                res.render("secrets")
-            }
-        })
-    });
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            res.render("/register")
+        }else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets")
+            })
+        }
+    })
 
  
     
@@ -62,27 +94,25 @@ app.post("/register", function(req,res){
 
 
 app.post("/login", function(req, res){
-    // prea informatia trasmisa in capurile cu username si password
-    const username = req.body.username;
-    const password = req.body.password;
+   
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
 
-    // acestea verifica daca exista persoana cu username si password in baza de data
-    User.findOne({email:username}, function(err,foundUser){
+    req.login(user, function(err){
         if(err){
             console.log(err);
         }else{
-            if(foundUser){
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-                    // result == true
-                    if(result === true){
-                        res.render("secrets")
-                    }
-                });
-                    
-                
-            }
+            passport.authenticate("local")(req,res, function(){
+                res.redirect("/secrets")
+            })
         }
     })
+
+
+
+
 })
 
 
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -11,26 +11,46 @@ var ejs = require("ejs");
 
 var mongoose = require("mongoose");
 
-var bcrypt = require("bcrypt");
+var session = require("express-session");
 
-var saltRounds = 10;
+var passport = require("passport");
+
+var passportLocalMongoose = require("passport-local-mongoose");
+
 var app = express();
 var port = 3000;
 app.use(express["static"]("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
-}));
+})); // initializare session
+
+app.use(session({
+  secret: 'Our little secret.',
+  resave: false,
+  saveUninitialized: false
+})); // initializare si folosire a passport
+
+app.use(passport.initialize());
+app.use(passport.session()); //mongoose 
+
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+mongoose.set("useCreateIndex", true);
 var userSchema = new mongoose.Schema({
   email: String,
   password: String
-}); // modelu mongoose
+}); // initalizare passport-local-mongoose
 
-var User = new mongoose.model("User", userSchema);
+userSchema.plugin(passportLocalMongoose); // modelu mongoose
+
+var User = new mongoose.model("User", userSchema); // initializare si folosire a passport-local-mongoose
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 app.get("/", function (req, res) {
   res.render("home");
 });
@@ -40,43 +60,43 @@ app.get("/login", function (req, res) {
 app.get("/register", function (req, res) {
   res.render("register");
 });
+app.get("/secrets", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.render("login");
+  }
+});
+app.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
 app.post("/register", function (req, res) {
-  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-    // Store hash in your password DB.
-    // prea info. transmise in register
-    var newUser = new User({
-      email: req.body.username,
-      password: hash
-    }); // salveaza info. in baza de date
-
-    newUser.save(function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    });
+  User.register({
+    username: req.body.username
+  }, req.body.password, function (err, user) {
+    if (err) {
+      console.log(err);
+      res.render("/register");
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/secrets");
+      });
+    }
   });
 });
 app.post("/login", function (req, res) {
-  // prea informatia trasmisa in capurile cu username si password
-  var username = req.body.username;
-  var password = req.body.password; // acestea verifica daca exista persoana cu username si password in baza de data
-
-  User.findOne({
-    email: username
-  }, function (err, foundUser) {
+  var user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  req.login(user, function (err) {
     if (err) {
       console.log(err);
     } else {
-      if (foundUser) {
-        bcrypt.compare(password, foundUser.password, function (err, result) {
-          // result == true
-          if (result === true) {
-            res.render("secrets");
-          }
-        });
-      }
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/secrets");
+      });
     }
   });
 });
